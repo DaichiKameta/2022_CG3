aa45a2e6865cda93dea239c264b35816cbe51c4f
DaichiKameta <k021g1323@g.neec.ac.jp>
2022-10-14 10:19:14 +0900

    01_06終了地点

--- Object3d.cpp
+++ Object3d.cpp
@@ -33,6 +33,9 @@ D3D12_INDEX_BUFFER_VIEW Object3d::ibView{};
 Object3d::VertexPosNormalUv Object3d::vertices[vertexCount];
 unsigned short Object3d::indices[indexCount];
 
+XMMATRIX Object3d::matBillboard = XMMatrixIdentity();
+XMMATRIX Object3d::matBillboardY = XMMatrixIdentity();
+
 void Object3d::StaticInitialize(ID3D12Device * device, int window_width, int window_height)
 {
 	// nullptrチェック
@@ -641,6 +644,7 @@ void Object3d::UpdateViewMatrix()
 
 	// カメラ回転行列
 	XMMATRIX matCameraRot;
+
 	// カメラ座標→ワールド座標系の変換行列
 	matCameraRot.r[0] = cameraAxisX;
 	matCameraRot.r[1] = cameraAxisY;
@@ -661,6 +665,33 @@ void Object3d::UpdateViewMatrix()
 
 	// ビュー行列に平行移動成分を設定
 	matView.r[3] = translation;
+
+#pragma region 全方向ビルボード行列の計算
+	// ビルボード行列
+	matBillboard.r[0] = cameraAxisX;
+	matBillboard.r[1] = cameraAxisY;
+	matBillboard.r[2] = cameraAxisZ;
+	matBillboard.r[3] = XMVectorSet(0, 0, 0, 1);
+
+#pragma region
+
+#pragma region Y軸回りビルボード行列の計算
+	// カメラX軸、Y軸、Z軸
+	XMVECTOR ybillCameraAxisX, ybillCameraAxisY, ybillCameraAxisZ;
+
+	// X軸は共通
+	ybillCameraAxisX = cameraAxisX;
+	// Y軸はワールド座標のY軸
+	ybillCameraAxisY = XMVector3Normalize(upVector);
+	// Z軸はX軸->Y軸の外積で求まる
+	ybillCameraAxisZ = XMVector3Cross(
+		ybillCameraAxisX,ybillCameraAxisY);
+	// Y軸回りビルボード行列
+	matBillboardY.r[0] = ybillCameraAxisX;
+	matBillboardY.r[1] = ybillCameraAxisY;
+	matBillboardY.r[2] = ybillCameraAxisZ;
+	matBillboardY.r[3] = XMVectorSet(0, 0, 0, 1);
+#pragma region
 }
 
 bool Object3d::Initialize()
@@ -684,6 +715,7 @@ bool Object3d::Initialize()
 	assert(SUCCEEDED(result));
 
 	return true;
+
 }
 
 void Object3d::Update()
@@ -701,6 +733,10 @@ void Object3d::Update()
 
 	// ワールド行列の合成
 	matWorld = XMMatrixIdentity(); // 変形をリセット
+	
+	//matWorld *= matBillboard;	// ビルボード行列を掛ける
+	matWorld *= matBillboardY;	// Y軸ビルボード行列を掛ける
+
 	matWorld *= matScale; // ワールド行列にスケーリングを反映
 	matWorld *= matRot; // ワールド行列に回転を反映
 	matWorld *= matTrans; // ワールド行列に平行移動を反映
